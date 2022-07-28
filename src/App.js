f71e3e477064515f71f53901354e4626e13d5581
Naufal-0123 <droid0536@gmail.com>
2022-07-28 13:16:39 +0700

    hapus file di my app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Header from "./Component/header";
-import Tes from "./Component/module/tes";
-import {Input, Button} from "./Component/name"
 function App () {
   return(
     <React.Fragment>
       <h1>Latihan Export Import</h1>
-      <Header/>
-      <Tes/>
-      <Input/>
-      <Button/>
+     
     </React.Fragment>
   );
 }
