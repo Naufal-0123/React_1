0470e7bb1c290d2b83382ce7977ff1301314bc8e
Naufal-0123 <droid0536@gmail.com>
2022-08-03 13:43:23 +0700

    belajar props2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import Layout from "./komponen/layout";
+import Button from "./komponen/button";
+import "./styles/styles.css"
+
 function App () {
   return(
     <React.Fragment>
-      <h1>Latihan Export Import</h1>
-     
+      <h1>Belajar Props2</h1>
+     <Layout title={'Nomor 1'}>
+      <h1>SMK MADINATUL QURAN</h1>
+     </Layout>
+     <Layout title={'Nomor 2'}>
+      <h1>SMK MADINATUL QURAN</h1>
+     </Layout>
+     <Layout title={'Nomor 3'}>
+      <h1>SMK MADINATUL QURAN</h1>
+     </Layout>
+     <Button onClick={()=>{
+      console.log("button ini di klik");
+     }} 
+     color="blue"
+     title={'simpan'} />
+     <Button onClick={()=>{
+      console.log("button ini di batalkan");
+     }} 
+     disable={true}
+     title={'batal'} />
+     <Button color="green" title={"update"} />
     </React.Fragment>
   );
 }
 
-export default App
+export default App;
---
+++ src/komponen/button.jsx
@@ -0,0 +1,49 @@
+// import React from 'react';
+
+// export default function Button({
+//     title, 
+//     color = "red", 
+//     onClick,
+//     name,
+//     id,
+//     disable = false,
+// }) {
+//     return (
+//         <React.Fragment>
+//             <button
+//                 onclick={onClick}
+//                 name={name}
+//                 id={id}
+//                 disable={disable}
+//                 style={{
+//                     backgroundColor: color,
+//                 }}
+//                 className="button"
+//             >
+//                 {title} 
+//             </button>
+//         </React.Fragment>
+//     )
+// }
+
+import React from 'react';
+
+export default function Button({
+    title,
+    color = "red",
+   ...props
+}) {
+    return (
+        <React.Fragment>
+            <button
+               {...props}
+                style={{
+                    backgroundColor: color,
+                }}
+                className="button"
+            >
+                {title}
+            </button>
+        </React.Fragment>
+    )
+}
