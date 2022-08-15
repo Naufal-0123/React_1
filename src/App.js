4070690ad8b4b59ee41e21302d921a371d4d82ae
Naufal-0123 <droid0536@gmail.com>
2022-08-15 14:22:44 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 function App () {
   return(
     <React.Fragment>
-      <h1>Latihan Export Import</h1>
+      <h1 className='bg-red-500'>Latihan Apaa</h1>
      
     </React.Fragment>
   );
