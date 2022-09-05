6a6ba463be7c798007876822d413c533b973531c
Naufal-0123 <droid0536@gmail.com>
2022-09-05 11:42:28 +0700

    ujian-react

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
-import React from "react"
+import React from 'react';
+import {Routes, Route, Link, NavLink, Navigate} from 'react-router-dom'
+
+function App () {
+  return(
+    <React.Fragment>
+      
+
+    <Routes>
+    
+      {/* <Route path='*'element={<Navigate to="/404" replace />} /> */}
+    </Routes>
+    </React.Fragment>
+    
+  );
+}
+
+export default App
