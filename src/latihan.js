68654271036a348a9ca90a6e24ee50b7898b8e16
Naufal-0123 <droid0536@gmail.com>
2022-09-28 11:15:30 +0700

    save

---
+++ src/latihan.js
@@ -0,0 +1,16 @@
+let score = 100;
+let result = ""
+
+if(score >= 90) {
+    result = "Selamat! Anda mendapatkan nilai A."
+} else if(score >= 80) {
+    result = "Anda mendapatkan nilai B."
+} else if(score >= 70) {
+    result = "Anda mendapatkan nilai C."
+} else if(score >= 60) {
+    result = "Anda mendapatkan nilai D."
+}  else if(score <= 60) {
+    result = "Anda mendapatkan nilai E."
+}
+
+console.log(result);
